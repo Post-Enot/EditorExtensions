44ed172191c001b3cb3fac3011ec364f32b95495
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2026-01-02 11:57:56 +0300

    Реализован LayerAttribute

---
+++ Scripts/Editor/LayerAttributePropertyDrawer.cs
@@ -0,0 +1,23 @@
+﻿using PostEnot.Toolkits;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace PostEnot.EditorExtensions.Editor
+{
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    internal sealed class LayerAttributePropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            if (property.propertyType != SerializedPropertyType.Integer)
+            {
+                return new Label($"Implement {nameof(LayerAttribute)} to int field.");
+            }
+            LayerField layerField = new(preferredLabel, property.intValue);
+            layerField.BindProperty(property);
+            layerField.AddToClassList(BaseField<int>.alignedFieldUssClassName);
+            return layerField;
+        }
+    }
+}
