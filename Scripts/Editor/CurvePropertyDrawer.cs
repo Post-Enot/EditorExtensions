ae79f576e2a278f462aa8fea2f606bb837b85e5f
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2026-01-02 10:41:10 +0300

    Реализован атрибут CurveAttribute

---
+++ Scripts/Editor/CurvePropertyDrawer.cs
@@ -0,0 +1,41 @@
+﻿using PostEnot.Toolkits;
+using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace PostEnot.EditorExtensions.Editor
+{
+    [CustomPropertyDrawer(typeof(CurveAttribute))]
+    internal class CurvePropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            CurveField curveField = new(preferredLabel);
+            curveField.BindProperty(property);
+            CurveAttribute curveAttribute = attribute as CurveAttribute;
+            curveField.ranges = Rect.MinMaxRect(
+                curveAttribute.MinX,
+                curveAttribute.MinY,
+                curveAttribute.MaxX,
+                curveAttribute.MaxY);
+            TrySetCurveColor(curveField, curveAttribute.HexColor);
+            curveField.AddToClassList(BaseField<AnimationCurve>.alignedFieldUssClassName);
+            return curveField;
+        }
+
+        private static void TrySetCurveColor(CurveField curveField, string hexColor)
+        {
+            if (ColorUtility.TryParseHtmlString(hexColor, out Color color))
+            {
+                Debug.Log("SET");
+                Type curveType = typeof(CurveField);
+                FieldInfo fieldInfo = curveType.GetField("m_CurveColor", BindingFlags.NonPublic | BindingFlags.Instance);
+                fieldInfo?.SetValue(curveField, color);
+                curveField.MarkDirtyRepaint();
+            }
+        }
+    }
+}
---
+++ Scripts/Runtime/CurveAttribute.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace PostEnot.Toolkits
+{
+    public sealed class CurveAttribute : PropertyAttribute
+    {
+        public CurveAttribute(string hexColor) => HexColor = hexColor;
+
+        public CurveAttribute(float minX, float minY, float maxX, float maxY, string hexColor = default)
+        {
+            MinX = minX;
+            MinY = minY;
+            MaxX = maxX;
+            MaxY = maxY;
+            HexColor = hexColor;
+        }
+
+        public float MinX { get; }
+        public float MinY { get; }
+        public float MaxX { get; }
+        public float MaxY { get; }
+        public string HexColor { get; }
+    }
+}
