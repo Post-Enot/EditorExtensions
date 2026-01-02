2bce1dff5640647b63b5175489b473ec8370e355
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2026-01-02 11:58:12 +0300

    Реализован TagAttribute

---
+++ Scripts/Editor/TagAttributePropertyDrawer.cs
@@ -0,0 +1,23 @@
+﻿using PostEnot.Toolkits;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace PostEnot.EditorExtensions.Editor
+{
+    [CustomPropertyDrawer(typeof(TagAttribute))]
+    internal sealed class TagAttributePropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            if (property.propertyType != SerializedPropertyType.String)
+            {
+                return new Label($"Implement {nameof(TagAttribute)} to string field.");
+            }
+            TagField tagField = new(preferredLabel, property.stringValue);
+            tagField.BindProperty(property);
+            tagField.AddToClassList(BaseField<int>.alignedFieldUssClassName);
+            return tagField;
+        }
+    }
+}
---
+++ Scripts/Runtime/TagAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace PostEnot.Toolkits
+{
+    public sealed class TagAttribute : PropertyAttribute
+    {
+        public TagAttribute() {}
+    }
+}
